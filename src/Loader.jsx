aa79dfab6b97091ca26e6834ba2f316636bfb81d
Aman Kushwaha <amankushwaha7284@gmail.com>
2025-08-20 10:58:05 +0530

    new project

---
+++ src/Loader.jsx
@@ -0,0 +1,9 @@
+export default function Loader() {
+  console.log('loader')
+  return (
+    <div className="loader-container">
+      <div className="loader"></div>
+      <p>Loading questions...</p>
+    </div>
+  );
+}
